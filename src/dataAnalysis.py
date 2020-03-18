055ae7de37755383ad3bfa63ff9dacefdcaa47ee
GuilhemTurbiau <popolepingouin@gmail.com>
2020-03-18 12:41:28 +0100

    Created data structures for corpus and adapted the code

--- src/dataAnalysis.py
+++ src/dataAnalysis.py
@@ -2,83 +2,146 @@ import json
 from os import walk
 import pandas as pds
 
-corpusFolder = "../data/nlp_project_corpus/corpus/fr/"
+# Contient une phrase et les étiquettes
+# Attributs :
+# - sentence :  la phrase (suite de mots)
+# - labels :    les étiquettes (liste de même longueur que sentence)
+class Data:
+    def __init__(self, sentence, labels):
+        self.sentence = sentence
+        self.labels = labels
+
+    # Itère sur les 3-grams (non uniques) de la phrase
+    def iterate3Grams(self):
+        for i in range(2, len(self.sentence)):
+            yield (self.sentence[i - 2], self.sentence[i - 1], self.sentence[i])
+
+
+# Classe qui contient les données d'un corpus et quelques méta-données précalculées
+# Attributs :
+# - name :                  le nom du corpus
+# - data :                  liste de Data
+# - nb3Grams :              nombre de 3-grams non uniques du corpus
+# - nbDistinct3Grams :      nombre de 3-grams uniques du corpus
+# - distinct3Grams :        ensemble des 3-grams uniques
+# - nbUniqueWords :         nombre de mots uniques du corpus
+# - uniqueWords :           ensemble des mots uniques du corpus
+class Corpus:
+    def __init__(self, name, data):
+        self.name = name
+        self.data = [Data(d[0], d[1]) for d in data]
+        
+        self.update3Grams()
+        self.updateDistinct3Grams()
+        self.updateUniqueWords()
+
+    # Itère sur les 3-grams (non uniques) du corpus, phrase par phrase
+    def iterate3Grams(self):
+        for d in self.data:
+            for n in d.iterate3Grams():
+                yield n
+
+    def update3Grams(self):
+        self.nb3Grams = 0
+        for n in self.iterate3Grams():
+            self.nb3Grams += 1
+
+    def updateDistinct3Grams(self):
+        self.nbDistinct3Grams = 0
+        self.distinct3Grams = set()
+        for n in self.iterate3Grams():
+            if not n in self.distinct3Grams:
+                self.distinct3Grams.add(n)
+                self.nbDistinct3Grams += 1
+
+    def updateUniqueWords(self):
+        self.nbUniqueWords = 0
+        self.uniqueWords = set()
+        for d in self.data:
+            for word in d.sentence:
+                if not word in self.uniqueWords:
+                    self.uniqueWords.add(word)
+                    self.nbUniqueWords += 1
+
+
 listeCorpus = []
-decodedData = []
+def init():
+    global listeCorpus
 
-for (_, _, files) in walk(corpusFolder):
-    files.remove("fr.pud.dev.json")
-    listeCorpus.extend(files)
+    def decodeData(file):
+        with open(file, 'r') as fp:
+            loaded_json = json.load(fp)
+        return json.JSONDecoder().decode(json.dumps(loaded_json))
 
+    corpusFolder = "../data/nlp_project_corpus/corpus/fr/"
+    corpus = []
+    listeCorpus = []
 
-def decodeData(file):
-    with open(file, 'r') as fp:
-        loaded_json = json.load(fp)
-    return json.JSONDecoder().decode(json.dumps(loaded_json))
+    for (_, _, files) in walk(corpusFolder):
+        files.remove("fr.pud.dev.json")
+        corpus.extend(files)
 
+    print("Initialization...")
+    for file in corpus:
+        print("\tPreparing corpus " + file + "...")
+        decodedData = decodeData(corpusFolder + file)
+        listeCorpus.append(Corpus(file, decodedData))
+        print("\tDone")
+    print("Done!")
 
-for file in listeCorpus:
-    decodedData.append(decodeData(corpusFolder + file))
-
+def getCorpusNames():
+    return [c.name for c in listeCorpus]
 
 def dataAnalysis():
     exampleSentence = []
     nbrSentence = []
     nbrSentenceElement = []
     nbrUniqueElement = []
-    for data in decodedData:
+    for corpus in listeCorpus:
+        data = corpus.data
         occur = {}
-        uniqueElement = set()
         firstSentence = []
         # recupere les 3 premiere phrases de chaque corpus
         for sentence_label in data[0:3]:
-            firstSentence.append(" ".join(sentence_label[0]))
+            firstSentence.append(" ".join(sentence_label.sentence))
         exampleSentence.append(firstSentence)
         # calcul le nombre d'element (mot, ponctuation, autre...)
         # de chaque corpus
         for sentence_label in data:
-            for word in sentence_label[0]:
-                uniqueElement.add(word)
+            for word in sentence_label.sentence:
                 occur.setdefault(word, 0)
                 occur[word] += 1
         nbrSentence.append(len(data))
         nbrSentenceElement.append(sum(occur.values()))
-        nbrUniqueElement.append(len(uniqueElement))
+        nbrUniqueElement.append(corpus.nbUniqueWords)
     return exampleSentence, nbrSentence, nbrSentenceElement, nbrUniqueElement
-
-def uniqueWord(data):
-    uniqueWord = set()
-    for sentence_label in data:
-        for word in sentence_label[0]:
-            uniqueWord.add(word)
-    return uniqueWord
 
 # renvoie les informations du calcul du pourcentage d'out-of-vocabulary words
 def computeOOV():
     oovPercent = []
     trainFst = []
     testSnd = []
-    uniqWordAllCorpus = []
-    for data in decodedData:
-        uniqWordAllCorpus.append(uniqueWord(data))
-    
-    for train, nameTrainCorpus in zip(uniqWordAllCorpus, listeCorpus):
-        lenUniqtrain = len(train)
-        for test, nameTestCorpus in zip(uniqWordAllCorpus, listeCorpus):
-            if nameTrainCorpus != nameTestCorpus and nameTestCorpus not in trainFst:
-                trainFst.append(nameTrainCorpus)
-                testSnd.append(nameTestCorpus)
-                oov = train - test
-                oovPercent.append( (len(oov) / (lenUniqtrain + len(test))) * 100 )
+
+    for train in listeCorpus:
+        for test in listeCorpus:
+            if train != test and test.name not in trainFst:
+                trainFst.append(train.name)
+                testSnd.append(test.name)
+                oov = test.uniqueWords - train.uniqueWords
+                oovPercent.append( (len(oov) / (train.nbUniqueWords + test.nbUniqueWords) * 100))
     return trainFst, testSnd, oovPercent
 
 """
 def computeKLdivergence():
+    # nbdataset : nombre de 3-grams dans l'ensemble concerné par l'appel
+    def probability(nbdataset, n):
+
+        return (nb + 1) / (truc + bidule * nbdataset)
+
+
     for train, trainCorpus in zip(decodedData, listeCorpus):
         for test, testCorpus in zip(decodedData, listeCorpus):
             if nameTrainCorpus != nameTestCorpus and nameTestCorpus not in trainFst:
-                
-
 
 computeKLdivergence()
 """
